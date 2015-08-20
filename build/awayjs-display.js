9d3e5b273f2d7fae967a23b69a5fc82a94cc42cf
80prozent <80prozent@differentdesign.de>
2015-08-20 23:11:50 +0200

    update build files

--- build/awayjs-display.js
+++ build/awayjs-display.js
@@ -3988,11 +3988,14 @@ var Timeline = (function () {
                     if (!child.adapter.isBlockedByScript()) {
                         if (child._iMatrix3D) {
                             child._iMatrix3D.identity();
-                            child._iMatrix3D = child._iMatrix3D;
+                            child.x = child._iMatrix3D.rawData[12];
+                            child.y = child._iMatrix3D.rawData[13];
+                            child._elementsDirty = true;
+                            child.pInvalidateHierarchicalProperties(HierarchicalProperties.SCENE_TRANSFORM);
                         }
                         if (child._iColorTransform) {
                             child._iColorTransform.clear();
-                            child._iColorTransform = child._iColorTransform;
+                            child.pInvalidateHierarchicalProperties(HierarchicalProperties.COLOR_TRANSFORM);
                         }
                         //this.name="";
                         child.masks = null;
