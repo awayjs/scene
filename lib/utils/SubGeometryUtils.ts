4665612362b4df0a549e9d216b694465e2fa8e18
bwhiting <ben.whiting@live.co.uk>
2015-08-26 09:01:44 +0100

    more hit testing optimizations using caching

--- lib/utils/SubGeometryUtils.ts
+++ lib/utils/SubGeometryUtils.ts
@@ -761,6 +761,8 @@ class SubGeometryUtils
 				id1 = nodes[k + 1];
 				id2 = nodes[k + 2];
 
+				if(id2 == index) continue;
+
 				ax = positions[id0 * posDim];
 				ay = positions[id0 * posDim + 1];
 				bx = positions[id1 * posDim];
@@ -841,6 +843,8 @@ class SubGeometryUtils
 			id0 = k + 2;
 			id1 = k + 1;
 			id2 = k + 0;
+
+			if(id2 == index) continue;
 
 			ax = positions[id0 * posDim];
 			ay = positions[id0 * posDim + 1];
