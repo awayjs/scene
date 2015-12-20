aeaee4539fad090daeaa4ca7ff4497d7bebb31a4
rob-bateman <rob@infiniteturtles.co.uk>
2015-12-20 02:50:36 +0000

    update tests

--- tests/textures/Single2DTextureTest.ts
+++ tests/textures/Single2DTextureTest.ts
@@ -1,9 +1,9 @@
-import BitmapImage2D		= require("awayjs-core/lib/data/BitmapImage2D");
+import BitmapImage2D		= require("awayjs-core/lib/image/BitmapImage2D");
 import Rectangle			= require("awayjs-core/lib/geom/Rectangle");
 import URLLoader			= require("awayjs-core/lib/net/URLLoader");
 import URLLoaderDataFormat	= require("awayjs-core/lib/net/URLLoaderDataFormat");
 import URLRequest			= require("awayjs-core/lib/net/URLRequest");
-import Event				= require("awayjs-core/lib/events/Event");
+import LoaderEvent			= require("awayjs-core/lib/events/LoaderEvent");
 import ParserUtils			= require("awayjs-core/lib/parsers/ParserUtils");
 import Debug				= require("awayjs-core/lib/utils/Debug");
 
@@ -11,48 +11,45 @@ import Single2DTexture		= require("awayjs-display/lib/textures/Single2DTexture")
 
 class Single2DTextureTest
 {
-
-	private mipLoader       : URLLoader;
-	private bitmapData      : BitmapImage2D;
-	private target          : Single2DTexture;
+	private mipLoader:URLLoader;
+	private bitmapData:BitmapImage2D;
+	private target:Single2DTexture;
 
 	constructor()
 	{
-
 		//---------------------------------------
 		// Load a PNG
 
-		var mipUrlRequest = new URLRequest( 'assets/1024x1024.png');
+		var mipUrlRequest = new URLRequest('assets/1024x1024.png');
 		this.mipLoader  = new URLLoader();
 		this.mipLoader.dataFormat = URLLoaderDataFormat.BLOB;
-		this.mipLoader.load( mipUrlRequest );
-		this.mipLoader.addEventListener( Event.COMPLETE , (e) => this.mipImgLoaded(e) );
+		this.mipLoader.load(mipUrlRequest);
+		this.mipLoader.addEventListener(LoaderEvent.LOAD_COMPLETE, (event:LoaderEvent) => this.mipImgLoaded(event));
 
 	}
 
-	private mipImgLoaded( e )
+	private mipImgLoaded(event:LoaderEvent)
 	{
 
-		var loader  : URLLoader        = <URLLoader > e.target;
-		var image : HTMLImageElement = ParserUtils.blobToImage(loader.data);
-		image.onload = ( event ) => this.onImageLoad( event );
+		var loader:URLLoader = event.target;
+		var image:HTMLImageElement = ParserUtils.blobToImage(loader.data);
+		image.onload = (event) => this.onImageLoad(event);
 	}
 
-	private onImageLoad (event)
+	private onImageLoad(event)
 	{
-		var image : HTMLImageElement = <HTMLImageElement> event.target;
+		var image:HTMLImageElement = <HTMLImageElement> event.target;
 
-		var rect    : Rectangle       = new Rectangle( 0 , 0 , image.width , image.height );
+		var rect:Rectangle = new Rectangle(0, 0, image.width, image.height);
 
-		console.log( 'Event' , image );
+		console.log('LoaderEvent', image);
 
-		this.bitmapData = new BitmapImage2D( image.width , image.height );
+		this.bitmapData = new BitmapImage2D(image.width , image.height);
 		this.bitmapData.draw(image);
 
 		this.target = new Single2DTexture(this.bitmapData);
 
-		Debug.log( 'BitmapImage2D'           , this.bitmapData );
-		Debug.log( 'Single2DTexture'       , this.target );
-
+		Debug.log('BitmapImage2D', this.bitmapData);
+		Debug.log('Single2DTexture', this.target);
 	}
 }
