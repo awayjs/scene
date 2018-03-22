7f6ca59aece3a3ed233a264cd0325b6fb78b67d5
Rob Bateman <rob@infiniteturtles.co.uk>
2018-03-22 03:16:43 +0000

    re-enable execute_dispose()

--- lib/managers/FrameScriptManager.ts
+++ lib/managers/FrameScriptManager.ts
@@ -104,13 +104,11 @@ export class FrameScriptManager
 
 	public static execute_dispose():void
 	{
-		/*
 		var len:number = this._queued_dispose.length;
 		for (var i:number = 0; i < len; i++)
 			this._queued_dispose[i].dispose();
 
 		this._queued_dispose.length = 0;
-		*/
 	}
 }
 export default FrameScriptManager;
