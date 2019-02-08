fb03b6ae63e5b12ae3dd09820b6d6259e7479a2f
Rob Bateman <rob@infiniteturtles.co.uk>
2019-02-08 03:29:48 +0000

    swith FNTGenerator to use shared context and view.target property

--- lib/text/FNTGenerator.ts
+++ lib/text/FNTGenerator.ts
@@ -36,16 +36,16 @@ export class FNTGenerator
 		//projection.originY = -1;
 
         //create the view
-		this._scene = new Scene(new DefaultRenderer(new SceneGraphPartition(root)));
-		this._scene.camera.projection = projection;
+		this._scene = new Scene(new DefaultRenderer(new SceneGraphPartition(root), new View(projection, this._stage)));
+		//this._scene.camera.projection = projection;
 		this._scene.disableMouseEvents = true;
-		this._scene.view.width = maxSize;
-        this._scene.view.height = maxSize;
+		//this._scene.view.width = maxSize;
+        //this._scene.view.height = maxSize;
 		(<PerspectiveProjection>this._scene.camera.projection).fieldOfView = Math.atan(maxSize/1000/2)*360/Math.PI;
 		//this._fillColor = this._fillColor;
 		//this._scene.renderer.view.backgroundAlpha = 0;//this._transparent? ( this._fillColor & 0xff000000 ) >>> 24 : 1;
         //this._scene.renderer.view.backgroundColor = 0x00000000;
-        this._scene.view.stage.container.style.display="NONE";
+        //this._scene.view.stage.container.style.display="NONE";
         
 
 		this._scene.renderer.renderableSorter = null;//new RenderableSort2D();
@@ -77,17 +77,19 @@ export class FNTGenerator
                     fntRenderSprite.x=-mipSize/2;
                     fntRenderSprite.y=-mipSize/2;
                     fntRenderSprite.scaleX=fntRenderSprite.scaleY=mipScale;
-                    this._scene.renderer.view.width = mipSize;
-                    this._scene.renderer.view.height = mipSize;
+                    //this._scene.renderer.view.width = mipSize;
+                    //this._scene.renderer.view.height = mipSize;
                     this._scene.renderer.view.backgroundColor = 0;
+                    this._scene.renderer.view.backgroundAlpha = 0.5;
                     (<PerspectiveProjection>this._scene.camera.projection).fieldOfView = Math.atan(mipSize/1000/2)*360/Math.PI;
                                           
                     console.log("width", PickGroup.getInstance(this._scene.view).getBoundsPicker(fntRenderSprite.partition).getBoxBounds(fntRenderSprite).width);
                     
                     outputBitmap=new BitmapImage2D(mipSize, mipSize, true, 0, true);
                     
-                    this._scene.view.clear();
-                    this._scene.renderer.queueSnapshot(outputBitmap);
+                    this._scene.renderer.view.target = outputBitmap;
+                    //this._scene.view.clear();
+                    //this._scene.renderer.queueSnapshot(outputBitmap);
                     this._scene.renderer.render();
 
                     outputBitmaps.push(outputBitmap);
@@ -100,15 +102,15 @@ export class FNTGenerator
                         fntRenderSprite.x=-mipSize/2;
                         fntRenderSprite.y=-mipSize/2;
                         fntRenderSprite.scaleX=fntRenderSprite.scaleY=mipScale;
-                        this._scene.renderer.view.width = mipSize;
-                        this._scene.renderer.view.height = mipSize;
+                        //this._scene.renderer.view.width = mipSize;
+                        //this._scene.renderer.view.height = mipSize;
                         this._scene.renderer.view.backgroundColor = ColorUtils.ARGBtoFloat32(1, (mipSize/maxSize)*255, 0,0);
                         (<PerspectiveProjection>this._scene.camera.projection).fieldOfView = Math.atan(mipSize/1000/2)*360/Math.PI;
                         
                         var mipBitmap:BitmapImage2D=new BitmapImage2D(mipSize, mipSize, true, 0, true);
-
-                        this._scene.view.clear();
-                        this._scene.renderer.queueSnapshot(mipBitmap);
+                        this._scene.renderer.view.target = mipBitmap;
+                        //this._scene.view.clear();
+                        //this._scene.renderer.queueSnapshot(mipBitmap);
                         this._scene.renderer.render();
                         outputBitmap.addMipLevel(mipBitmap);
                         
