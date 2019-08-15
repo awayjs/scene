7ebcd26902f91364a6adbede8c6d3b09907ef3be
Rob Bateman <rob.bateman@gmail.com>
2019-08-15 23:48:45 +0100

    tidy up FNTGenerator

--- lib/text/FNTGenerator.ts
+++ lib/text/FNTGenerator.ts
@@ -1,10 +1,10 @@
 import { Font } from './Font';
 import { TesselatedFontTable } from './TesselatedFontTable';
-import {ColorTransform, Matrix, Rectangle, Point, ColorUtils, PerspectiveProjection, CoordinateSystem} from "@awayjs/core";
+import {ColorTransform, Matrix, Rectangle, Point, ColorUtils, PerspectiveProjection, CoordinateSystem, Transform, Vector3D} from "@awayjs/core";
 
 import {Stage, ImageSampler, BitmapImage2D, _Stage_BitmapImage2D, BlendMode} from "@awayjs/stage";
 
-import {DefaultRenderer} from "@awayjs/renderer";
+import {DefaultRenderer, RenderGroup, RendererType} from "@awayjs/renderer";
 
 import { MethodMaterial} from '@awayjs/materials';
 import {DisplayObject} from "../display/DisplayObject";
@@ -18,51 +18,45 @@ import { Shape } from '@awayjs/graphics';
 
 export class FNTGenerator
 {
-
-	private _scene:Scene;
+    private _root:DisplayObjectContainer;
+	private _renderer:DefaultRenderer;
 	private _stage:Stage;
 
 	constructor(stage)
 	{
-        this._stage=stage;
-    }
+        this._stage = stage;
 
-
-	private createScene(root:DisplayObjectContainer, maxSize:number)
-	{
 		//create the projection
 		var projection = new PerspectiveProjection();
 		projection.coordinateSystem = CoordinateSystem.RIGHT_HANDED;
-		//projection.originX = -1;
-		//projection.originY = -1;
+		projection.originX = -1;
+		projection.originY = 1;
 
         //create the view
-		this._scene = new Scene(new SceneGraphPartition(root));
-		this._scene.camera.projection = projection;
-		this._scene.disableMouseEvents = true;
-		this._scene.view.width = maxSize;
-        this._scene.view.height = maxSize;
-		(<PerspectiveProjection>this._scene.camera.projection).fieldOfView = Math.atan(maxSize/1000/2)*360/Math.PI;
-		//this._fillColor = this._fillColor;
-		//this._scene.renderer.view.backgroundAlpha = 0;//this._transparent? ( this._fillColor & 0xff000000 ) >>> 24 : 1;
-        //this._scene.renderer.view.backgroundColor = 0x00000000;
-        this._scene.view.stage.container.style.display="NONE";
+        this._root = new DisplayObjectContainer()
+        this._renderer = <DefaultRenderer> RenderGroup.getInstance(new View(), RendererType.DEFAULT).getRenderer(new SceneGraphPartition(this._root));
+        this._root.partition = this._renderer.partition;
+
+        //setup the projection
+        this._renderer.view.projection = projection;
+        this._renderer.view.projection.transform = new Transform();
+		this._renderer.view.projection.transform.moveTo(0, 0, -1000);
+		this._renderer.view.projection.transform.lookAt(new Vector3D());
+        
+        //hide the html container
+        this._renderer.view.stage.container.style.display = "NONE";
         
 
-		this._scene.renderer.renderableSorter = null;//new RenderableSort2D();
+		this._renderer.renderableSorter = null;//new RenderableSort2D();
 
-	}
-    public generate(font:Font, maxSize:number, fontSize:number, padding:number):any{
-        
-            var root:DisplayObjectContainer = new DisplayObjectContainer();
-            
-			if (!this._scene)
-                this.createScene(root, maxSize);
+    }
 
+    public generate(font:Font, maxSize:number, fontSize:number, padding:number):any
+    {
             let outputBitmap:BitmapImage2D;
             var outputBitmaps:BitmapImage2D[]=[];
             var mipSize:number;
-            var pixelRatio:number = this._scene.renderer.view.stage.context.pixelRatio;
+            var pixelRatio:number = this._renderer.view.stage.context.pixelRatio;
 
             for(var i:number=0; i<font.font_styles.length; i++) {
 
@@ -74,26 +68,26 @@ export class FNTGenerator
 
                     var fntRenderSprite:Sprite=new Sprite();
                     fntRenderSprite.graphics.addShape(shapes[s]);
-                    root.removeChildren(0, root.numChildren);
-                    root.addChild(fntRenderSprite);
+                    this._root.removeChildren(0, this._root.numChildren);
+                    this._root.addChild(fntRenderSprite);
                     
-                    fntRenderSprite.x = -maxSize/2;
-                    fntRenderSprite.y = -maxSize/2;
-                    this._scene.camera.projection.scale = 1000/maxSize;
+                    // fntRenderSprite.x = -maxSize/2;
+                    // fntRenderSprite.y = -maxSize/2;
+                    this._renderer.view.projection.scale = 1000/maxSize;
                                           
 
                     while(mipSize >= 1) {
 
-                        this._scene.renderer.view.width = mipSize/pixelRatio;
-                        this._scene.renderer.view.height = mipSize/pixelRatio;
-                        this._scene.renderer.view.backgroundAlpha = 0;
-                        this._scene.renderer.view.backgroundColor = ColorUtils.ARGBtoFloat32(1, (mipSize/maxSize)*255, 0,0);
+                        this._renderer.view.width = mipSize/pixelRatio;
+                        this._renderer.view.height = mipSize/pixelRatio;
+                        this._renderer.view.backgroundAlpha = 0;
+                        this._renderer.view.backgroundColor = ColorUtils.ARGBtoFloat32(1, (mipSize/maxSize)*255, 0,0);
                         
                         var mipBitmap:BitmapImage2D = new BitmapImage2D(mipSize, mipSize, true, 0, true);
 
-                        this._scene.view.clear();
-                        this._scene.renderer.queueSnapshot(mipBitmap);
-                        this._scene.renderer.render();
+                        this._renderer.queueSnapshot(mipBitmap);
+                        //this._scene.view.stage.context.configureBackBuffer(mipSize/2, mipSize/2, 0, true);
+                        this._renderer.render();
 
                         if (mipSize == maxSize) {
                             outputBitmap = mipBitmap
