f8b6773f2eeb0d6c33904d2e164c3439d8671144
Rob Bateman <rob.bateman@gmail.com>
2019-08-15 16:27:01 +0100

    clean up FNTGenerator, corrected for devicePixelRatio > 1

--- lib/text/FNTGenerator.ts
+++ lib/text/FNTGenerator.ts
@@ -14,6 +14,7 @@ import { SceneGraphPartition } from '../partition/SceneGraphPartition';
 import {Scene} from "../Scene";
 import { View, PickGroup } from '@awayjs/view';
 import { Sprite } from '../display/Sprite';
+import { Shape } from '@awayjs/graphics';
 
 export class FNTGenerator
 {
@@ -60,59 +61,50 @@ export class FNTGenerator
 
             let outputBitmap:BitmapImage2D;
             var outputBitmaps:BitmapImage2D[]=[];
-            var mipSize:number=maxSize;
-            var mipScale=1;
-            for(var i:number=0; i<font.font_styles.length; i++){
-                var shapes = (<TesselatedFontTable>font.font_styles[i]).generateFNTTextures(padding, fontSize, maxSize);
-                for(var s=0; s<shapes.length; s++){
+            var mipSize:number;
+            var pixelRatio:number = this._scene.renderer.view.stage.context.pixelRatio;
 
-                    mipScale=1;
-                    mipSize=maxSize;
+            for(var i:number=0; i<font.font_styles.length; i++) {
+
+                var shapes:Shape[] = (<TesselatedFontTable>font.font_styles[i]).generateFNTTextures(padding, fontSize, maxSize);
+
+                for(var s = 0; s < shapes.length; s++) {
+
+                    mipSize = maxSize;
 
                     var fntRenderSprite:Sprite=new Sprite();
                     fntRenderSprite.graphics.addShape(shapes[s]);
                     root.removeChildren(0, root.numChildren);
                     root.addChild(fntRenderSprite);
                     
-                    fntRenderSprite.x=-mipSize/2;
-                    fntRenderSprite.y=-mipSize/2;
-                    fntRenderSprite.scaleX=fntRenderSprite.scaleY=mipScale;
-                    this._scene.renderer.view.width = mipSize;
-                    this._scene.renderer.view.height = mipSize;
-                    this._scene.renderer.view.backgroundColor = 0;
-                    (<PerspectiveProjection>this._scene.camera.projection).fieldOfView = Math.atan(mipSize/1000/2)*360/Math.PI;
+                    fntRenderSprite.x = -maxSize/2;
+                    fntRenderSprite.y = -maxSize/2;
+                    this._scene.camera.projection.scale = 1000/maxSize;
                                           
-                    console.log("width", PickGroup.getInstance(this._scene.view).getBoundsPicker(fntRenderSprite.partition).getBoxBounds(fntRenderSprite).width);
-                    
-                    outputBitmap=new BitmapImage2D(mipSize, mipSize, true, 0, true);
-                    
-                    this._scene.view.clear();
-                    this._scene.renderer.queueSnapshot(outputBitmap);
-                    this._scene.renderer.render();
 
-                    outputBitmaps.push(outputBitmap);
-                    (<TesselatedFontTable>font.font_styles[i]).addFNTChannel(outputBitmap);
+                    while(mipSize >= 1) {
 
-                    while(mipSize>=2){
-                        mipSize*=0.5;
-                        mipScale*=0.5;
-
-                        fntRenderSprite.x=-mipSize/2;
-                        fntRenderSprite.y=-mipSize/2;
-                        fntRenderSprite.scaleX=fntRenderSprite.scaleY=mipScale;
-                        this._scene.renderer.view.width = mipSize;
-                        this._scene.renderer.view.height = mipSize;
+                        this._scene.renderer.view.width = mipSize/pixelRatio;
+                        this._scene.renderer.view.height = mipSize/pixelRatio;
+                        this._scene.renderer.view.backgroundAlpha = 0;
                         this._scene.renderer.view.backgroundColor = ColorUtils.ARGBtoFloat32(1, (mipSize/maxSize)*255, 0,0);
-                        (<PerspectiveProjection>this._scene.camera.projection).fieldOfView = Math.atan(mipSize/1000/2)*360/Math.PI;
                         
-                        var mipBitmap:BitmapImage2D=new BitmapImage2D(mipSize, mipSize, true, 0, true);
+                        var mipBitmap:BitmapImage2D = new BitmapImage2D(mipSize, mipSize, true, 0, true);
 
                         this._scene.view.clear();
                         this._scene.renderer.queueSnapshot(mipBitmap);
                         this._scene.renderer.render();
-                        outputBitmap.addMipLevel(mipBitmap);
-                        
+
+                        if (mipSize == maxSize) {
+                            outputBitmap = mipBitmap
+                            outputBitmaps.push(outputBitmap);
+                            (<TesselatedFontTable>font.font_styles[i]).addFNTChannel(outputBitmap);
+                        } else {
+                            outputBitmap.addMipLevel(mipBitmap);
+                        }
+
                         //outputBitmaps.push(mipBitmap);
+                        mipSize *= 0.5;
                     }
                 }
                 //this._bitmapFontTable.addMaterial(mat);
