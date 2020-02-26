100b58d905401683eba638a90ecfb05d07a9b4a4
80prozent <80prozent@differentdesign.de>
2020-02-26 19:02:32 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.debug("AwayJS - Scene - 0.12.66");
+console.debug("AwayJS - Scene - 0.12.67");
 export {IDisplayObjectAdapter} from "./lib/adapters/IDisplayObjectAdapter";
 export {IMovieClipAdapter} from "./lib/adapters/IMovieClipAdapter";
 
