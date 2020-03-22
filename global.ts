d858ae5344e85b584c5801a11fb147c05c781d83
80prozent <80prozent@differentdesign.de>
2020-03-22 23:14:03 +0100

    remove global.ts

--- global.ts
+++
@@ -1,1 +0,0 @@
-declare type StringMap<T> = {[key: string]: T};
--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 console.debug("AwayJS - Scene - 0.13.7");
 
-import "./global";
-
 export {IDisplayObjectAdapter} from "./lib/adapters/IDisplayObjectAdapter";
 export {IMovieClipAdapter} from "./lib/adapters/IMovieClipAdapter";
 
