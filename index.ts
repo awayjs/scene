f11278582b958942fb686adc1d26c6046f36c2b1
eXponenta <rondo.devil@gmail.com>
2020-05-10 20:06:16 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayJS - Scene - 0.13.23");
+console.debug("AwayJS - Scene - 0.13.24");
 
 export {IDisplayObjectAdapter} from "./lib/adapters/IDisplayObjectAdapter";
 export {IMovieClipAdapter} from "./lib/adapters/IMovieClipAdapter";
