04998162e7a7fa5e996f91c4db777375b76d9861
Exponenta <rondo.devil@gmail.com>
2020-08-06 15:07:49 +0300

    Format

--- lib/text/Font.ts
+++ lib/text/Font.ts
@@ -1,9 +1,9 @@
-import {AssetBase} from "@awayjs/core";
+import { AssetBase } from "@awayjs/core";
 
-import {IFontTable} from "./IFontTable";
-import {TesselatedFontTable} from "./TesselatedFontTable";
-import {BitmapFontTable} from "./BitmapFontTable";
-import { FontStyleName } from './FontStyleName';
+import { IFontTable } from "./IFontTable";
+import { TesselatedFontTable } from "./TesselatedFontTable";
+import { BitmapFontTable } from "./BitmapFontTable";
+import { FontStyleName } from "./FontStyleName";
 
 /**
  * Font is a container for FontTables.
@@ -11,99 +11,95 @@ import { FontStyleName } from './FontStyleName';
  *
  *
  */
-export class Font extends AssetBase
-{
-	public static assetType:string = "[asset Font]";
+export class Font extends AssetBase {
+  public static assetType: string = "[asset Font]";
 
-	private _font_styles:Array<IFontTable> = [];
+  private _font_styles: Array<IFontTable> = [];
 
-	/**
-	 * Creates a new TesselatedFont object
-	 */
-	constructor()
-	{
-		super();
-	}
-	public fontName:string=null;
-	public regularFontStyle:IFontTable=null;
+  /**
+   * Creates a new TesselatedFont object
+   */
+  constructor() {
+    super();
+  }
+  public fontName: string = null;
+  public regularFontStyle: IFontTable = null;
 
-	// ------------ dummys for as3web:
+  // ------------ dummys for as3web:
 
-	public static registerFont(any){
-		
-	}
-	// ------------
-	public get font_styles():Array<IFontTable>
-	{
-		return this._font_styles;
-	}
-	/**
-	 *
-	 */
-	public get assetType():string
-	{
-		return Font.assetType;
-	}
-	/**
-	 *
-	 */
-	public dispose():void
-	{
-
-		var len:number = this._font_styles.length;
-		for (var i:number = 0; i < len; ++i) {
-			this._font_styles[i].dispose();
-		}
-		this._font_styles.length=0;
+  public static registerFont(any) {}
+  // ------------
+  public get font_styles(): Array<IFontTable> {
+    return this._font_styles;
+  }
+  /**
+   *
+   */
+  public get assetType(): string {
+    return Font.assetType;
+  }
+  /**
+   *
+   */
+  public dispose(): void {
+    var len: number = this._font_styles.length;
+    for (var i: number = 0; i < len; ++i) {
+      this._font_styles[i].dispose();
     }
-    public replace_font_table(style_name:FontStyleName, table:IFontTable)
-	{
-		var len:number = this._font_styles.length;
-		for (var i:number = 0; i < len; ++i) {
-			if(this._font_styles[i].name==style_name){
-				/*if(style_name!=FontStyleName.STANDART){
+    this._font_styles.length = 0;
+  }
+  public replace_font_table(style_name: FontStyleName, table: IFontTable) {
+    var len: number = this._font_styles.length;
+    for (var i: number = 0; i < len; ++i) {
+      if (this._font_styles[i].name == style_name) {
+        /*if(style_name!=FontStyleName.STANDART){
 					this._font_styles[i].fallbackTable=this.regularFontStyle;
 				}*/
-				this._font_styles[i]=table;
-			}
-        }
-	}
-	/**
-	 *Get a font-table for a specific name, or create one if it does not exists.
-	 */
-	public get_font_table(style_name:FontStyleName, assetType:string=TesselatedFontTable.assetType, openTypeFont:any=null, createTable:boolean=true):IFontTable
-	{
-		var len:number = this._font_styles.length;
+        this._font_styles[i] = table;
+      }
+    }
+  }
+  /**
+   *Get a font-table for a specific name, or create one if it does not exists.
+   */
+  public get_font_table(
+    style_name: FontStyleName,
+    assetType: string = TesselatedFontTable.assetType,
+    openTypeFont: any = null,
+    createTable: boolean = true
+  ): IFontTable {
+    var len: number = this._font_styles.length;
 
-		/*if(this.regularFontStyle==null){			
+    /*if(this.regularFontStyle==null){			
 			for (var i:number = 0; i < len; ++i) {
 				if((this._font_styles[i].assetType==assetType)&&(this._font_styles[i].name==FontStyleName.STANDART)){
 					this.regularFontStyle=this._font_styles[i];
 				}
 			}
 		}*/
-		//console.log("font name", this.name, style_name);
-		for (var i:number = 0; i < len; ++i) {
-			if((this._font_styles[i].assetType==assetType)&&(this._font_styles[i].name==style_name)){
-				/*if(style_name!=FontStyleName.STANDART){
+    //console.log("font name", this.name, style_name);
+    for (var i: number = 0; i < len; ++i) {
+      if (
+        this._font_styles[i].assetType == assetType &&
+        this._font_styles[i].name == style_name
+      ) {
+        /*if(style_name!=FontStyleName.STANDART){
 					this._font_styles[i].fallbackTable=this.regularFontStyle;
 				}*/
-				return this._font_styles[i];
-			}
-        }
-        if(!createTable)
-            return null;
-		var font_style:IFontTable=null;
-		if(assetType==TesselatedFontTable.assetType){
-			font_style = new TesselatedFontTable(openTypeFont);
-		}
-		else if(assetType==BitmapFontTable.assetType){
-			font_style = new BitmapFontTable();
-		}
-		font_style.name=<string>style_name;
-		font_style.font=this;
-        this._font_styles.push(font_style);
-        /*
+        return this._font_styles[i];
+      }
+    }
+    if (!createTable) return null;
+    var font_style: IFontTable = null;
+    if (assetType == TesselatedFontTable.assetType) {
+      font_style = new TesselatedFontTable(openTypeFont);
+    } else if (assetType == BitmapFontTable.assetType) {
+      font_style = new BitmapFontTable();
+    }
+    font_style.name = <string>style_name;
+    font_style.font = this;
+    this._font_styles.push(font_style);
+    /*
 		if(style_name==FontStyleName.STANDART){
 			this.regularFontStyle=font_style;			
 			for (var i:number = 0; i < len; ++i) {
@@ -116,8 +112,7 @@ export class Font extends AssetBase
 			font_style.fallbackTable=this.regularFontStyle;
         }
         */
-	
-		return font_style;
-	}
 
+    return font_style;
+  }
 }
