b88d2588afc48c0e8ee6549d21ab647061edae24
Dzmitry Dranitski <dranitski.d.a@gmail.com>
2020-12-04 23:18:44 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayJS - Scene - 0.13.123");
+console.debug("AwayJS - Scene - 0.13.124");
 
 export { IDisplayObjectAdapter } from './lib/adapters/IDisplayObjectAdapter';
 export { IMovieClipAdapter } from './lib/adapters/IMovieClipAdapter';
