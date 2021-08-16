fc4345bbe1fff9a797af4febc9001b9d5ac4e2b8
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-08-16 18:51:33 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Scene - 0.13.221');
+console.debug('AwayJS - Scene - 0.13.222');
 
 export { IDisplayObjectAdapter } from './lib/adapters/IDisplayObjectAdapter';
 export { IMovieClipAdapter } from './lib/adapters/IMovieClipAdapter';
