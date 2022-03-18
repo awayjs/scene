bd957f9cfedc48c18531a814e8265890281d5209
Konstantin Timoshenko <konstantin.t@deovr.com>
2022-03-18 20:51:13 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Scene - 0.13.236');
+console.debug('AwayJS - Scene - 0.13.237');
 
 export { IDisplayObjectAdapter } from './lib/adapters/IDisplayObjectAdapter';
 export { IMovieClipAdapter } from './lib/adapters/IMovieClipAdapter';
