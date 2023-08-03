7038faeccc5503e8a55daaa4c0b2b5fdd1614bff
Rob Bateman <rob@infiniteturtles.co.uk>
2023-08-03 01:06:48 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Scene - 0.13.259');
+console.debug('AwayJS - Scene - 0.13.260');
 
 export { IDisplayObjectAdapter } from './lib/adapters/IDisplayObjectAdapter';
 export { IMovieClipAdapter } from './lib/adapters/IMovieClipAdapter';
