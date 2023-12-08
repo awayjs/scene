646ae08d0b00cca1babeaf96a6010c602cc4c2d0
nosamu <71368227+n0samu@users.noreply.github.com>
2023-12-07 19:30:57 -0600

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Scene - 0.13.264');
+console.debug('AwayJS - Scene - 0.13.265');
 
 export { IDisplayObjectAdapter } from './lib/adapters/IDisplayObjectAdapter';
 export { IMovieClipAdapter } from './lib/adapters/IMovieClipAdapter';
