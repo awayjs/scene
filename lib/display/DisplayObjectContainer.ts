b41401dcf30f5574ced6d60d6ebe84f5822af816
Rob Bateman <rob@infiniteturtles.co.uk>
2025-02-05 13:36:41 +0000

    ensure ADD_CHILD_AT fires even when parent is the same, to update ContainerNode

--- lib/display/DisplayObjectContainer.ts
+++ lib/display/DisplayObjectContainer.ts
@@ -164,17 +164,6 @@ export class DisplayObjectContainer extends DisplayObject implements IRenderCont
 	 *              list.
 	 */
 	public addChildAt(child: DisplayObject, index: number): DisplayObject {
-		if (child.parent && child.parent == this) {
-			const childIndex: number = this.getChildIndex(child);
-
-			if (childIndex != index && index < this._children.length) {
-				this._children.splice(childIndex, 1);
-				this._children.splice(index, 0, child);
-			}
-
-			return child;
-		}
-
 		if (child.parent)
 			child.parent.removeChild(child);
 
